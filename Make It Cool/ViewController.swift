876576f7b3c46493cd5d99469bbf82349ad30454
Denys Zahorskyi <zahorskyi.lawyer@gmail.com>
2022-11-15 20:25:19 +0200

    Initial Commit

---
+++ Make It Cool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Make It Cool
+//
+//  Created by mac mini on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
